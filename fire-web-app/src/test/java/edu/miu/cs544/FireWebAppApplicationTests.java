a80f5bca89f5426c1dc6bf72f79ec956b713c233
osalam <tangbinhlam@gmail.com>
2020-02-02 15:33:47 -0600

    [fire-web-app] add spring mvc web app

---
+++ fire-web-app/src/test/java/edu/miu/cs544/FireWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs544;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
