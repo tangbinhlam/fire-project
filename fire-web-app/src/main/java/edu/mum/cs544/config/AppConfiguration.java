4f71ee647ce7369dc3accad9981d57ced26923eb
osalam <tangbinhlam@gmail.com>
2020-02-02 21:41:24 -0600

    [fire-web-app] add config eureka client

---
+++ fire-web-app/src/main/java/edu/mum/cs544/config/AppConfiguration.java
@@ -0,0 +1,9 @@
+package edu.mum.cs544.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class AppConfiguration {
+}
